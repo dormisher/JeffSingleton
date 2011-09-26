2d5ce32b00d6e912ec8149239990d1f580cc6411
Dan Ormisher <dormisher@gmail.com>
2011-09-26 13:03:06 +0100

    added contact page

---
+++ JeffSingleton/Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace JeffSingleton.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Contact/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
